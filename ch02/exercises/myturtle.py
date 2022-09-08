02ee49634d3d7a4f623630bbf35ad17a6293cbf9
Brianna Jules <28julesb@gmail.com>
2022-09-08 00:31:17 +0000

    9/7/22

---
+++ ch02/exercises/myturtle.py
@@ -0,0 +1,30 @@
+import turtle
+my_turtle = turtle.Turtle()
+
+my_screen = turtle.Screen()
+turtle.shape("turtle")
+turtle.color("purple")
+turtle.forward(50)
+turtle.right(90)
+turtle.forward(50)
+turtle.right(90)
+turtle.forward(50)
+turtle.right(90)
+turtle.forward(50)
+
+turtle.color("red")
+turtle.penup()
+turtle.forward(50)
+turtle.pendown()
+turtle.forward(50)
+turtle.right(90)
+turtle.forward(50)
+turtle.right(90)
+turtle.forward(50)
+turtle.right(90)
+turtle.forward(50)
+
+
+window = turtle.Screen()
+
+window.exitonclick()
