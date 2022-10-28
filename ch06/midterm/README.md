2cd13014964c97cdb88de01aee819f70a1b32302
Brianna Jules <28julesb@gmail.com>
2022-10-28 23:08:00 +0000

    midterm done

--- ch06/midterm/README.md
+++ ch06/midterm/README.md
@@ -1,9 +1,10 @@
 # CS110 Midterm Exam
 
 ## SHORT DESCRIPTION *(1 or 2 sentences describing your program)*
-
+My program draws a circle and then draws 4 equal squares. After it draws a triangle inside the circle.
 ## KNOWN BUGS AND INCOMPLETE PARTS *(list any known bugs or non-working parts)*
-
+Nope
 ## REFERENCES *(any resources you use outside of class materials)*
-
+Nope
 ## MISCELLANEOUS COMMENTS *(anything else you would like the grader to know)*
+Nope
--- ch06/midterm/main.py
+++ ch06/midterm/main.py
@@ -1,51 +1,83 @@
 import turtle
-window = turtle.Screen()
-window.bgcolor("lightblue")
-turt = turtle.Turtle()
 
-a = -10
-b = 20
-z = 0
-x = 100
-y = 90
-def draw_to_right(a, s):
-  return a + s
-right = draw_to_right(a, 125)
-def draw_down(b, s):
-  return b - s
-down = draw_down(b, 150)
+
+A = -10
+B = 20
+X = 100
+Y = 90
+
+
+def draw_to_right(A, s):
+  '''
+   A: (constant) x-coordinate for squares
+   s: (variable) varible added to A for coordinate to shift 
+   right
+  '''
+  return A + s
+def draw_down(B, s):
+  '''
+   B: (constant) y-coordinate for squares
+   s: (variable) varible added to A for coordinate to shift 
+   down
+  '''
+  return B - s
 def main():
+ turt = turtle.Turtle()
+ window = turtle.Screen()
+ window.bgcolor("lightblue")
+ right = draw_to_right(A, 125)
+ down = draw_down(B,150)
  circledrawing(turt)
- square_pos(a,b)
- draw_triangle()
+ square_pos(A,B, turt, down, right)
+ draw_triangle(turt)
+ window.exitonclick()
 def circledrawing(turt):
+  '''
+   turt: (variable) turtle
+  '''
   turt.penup()
   turt.goto(0, -100)
   turt.pendown()
   turt.circle(100)
-def square_pos(a,b):
+def square_pos(A,B, turt, down, right):
+  '''
+   A: (constant) x-coordinate for squares
+   B: (constant) y-coordinate for squares
+   turt: (variable) turtle
+   down: (variable) coordinate to move down from A
+   right: (variable) coordinate to move to the right from B
+  '''
   turt.penup()
-  turt.goto(a,b)
+  turt.goto(A,B)
   turt.pendown()
-  draw_square(x,y)
+  draw_square(X,Y,turt)
   turt.penup()
-  turt.goto(right,b)
+  turt.goto(right,B)
   turt.pendown()
-  draw_square(x,y)
-  turt.penup()
-  turt.goto(a,down)
+  draw_square(X,Y,turt)
+  turt.penup() 
+  turt.goto(A,down)
   turt.pendown()
-  draw_square(x,y)
+  draw_square(X,Y,turt)
   turt.penup()
   turt.goto(right,down)
   turt.pendown()
-  draw_square(x,y)
-def draw_square(x,y):
+  draw_square(X,Y,turt)
+def draw_square(X,Y,turt):
+  '''
+   X: (constant) foward distance
+   Y: (constant) left angle
+   turt: (variable) turtle
+  '''
+  z = 0
   for z in range(4):
-   turt.left(y)
-   turt.forward(x)
+   turt.left(Y)
+   turt.forward(X)
    z = z + 1
-def draw_triangle():
+def draw_triangle(turt):
+  '''
+   turt: (variable) turtle
+  '''
   turt.penup()
   turt.goto(-90,-55)
   turt.pendown()
@@ -57,4 +89,4 @@ def draw_triangle():
   
 main()
 
-window.exitonclick()
+
