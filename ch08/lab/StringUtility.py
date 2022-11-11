eb84618436100ff419e88aa5f3d3fd65f226b15d
Brianna Jules <28julesb@gmail.com>
2022-11-11 23:32:16 +0000

    ch08 lab 2

--- ch08/lab/StringUtility.py
+++ ch08/lab/StringUtility.py
@@ -1,9 +1,20 @@
 class StringUtility:
   def __init__(self, string):
     self.string = string
+    '''
+    intitializes class
+    string: (var) instance variable
+    '''
+    
   def __str__(self):
     return self.string
+    '''
+    returns string
+    '''
   def vowels(self):
+    '''
+    self: (var) self.string is checked for vowels
+    '''
     count = 0
     vowels =  ["A","a","E","e","i","I","o","O","u","U"] 
     for alphabet in self.string:
@@ -15,21 +26,35 @@ class StringUtility:
        count = count
     return str(count)
   def bothEnds(self):
+    '''
+    self: (var) adds first 2 and last 2 letters together from self.string
+    '''
     if len(self.string)>2:
       return self.string[0] + self.string[1] + self.string[-2] + self.string[-1]
     else:
       return ""
   def fixStart(self):
-    if len(self.string)>1:
-      newstr = self.string[1:]
-      newstr = newstr.replace(self.string[0],"*" )
-      newstr2 = self.string[0] + newstr
-      return newstr2
-    else:
-      return self.string
+     '''
+    self: (var) replaces letters in self.string and returns a new 
+    string
+    '''
+     if len(self.string)>1:
+       newstr = self.string[1:]
+       newstr = newstr.replace(self.string[0],"*" )
+       newstr2 = self.string[0] + newstr
+       return newstr2
+     else:
+       return self.string
   def asciiSum(self):
+    '''
+    self: (var) return sum of ascii numbers from self.string
+    '''
     return sum(map(ord,self.string))
+
   def cipher(self):
+    '''
+    self: (var) creates a new string from self.string
+    '''
     Message = ""
     capalphabet_list = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
     loweralp = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
